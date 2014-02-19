98aab81f7a7908ba447edea2227ec7fc5005f80b
David Lordan <110326755@umail.ucc.ie>
2014-02-19 13:31:10 +0000

    Update and rename SOS.html to SOS.php

---
+++ SOS.php
@@ -0,0 +1,47 @@
+<?php
+
+ include_once'func.php';
+
+
+
+head('SOS');
+
+
+?>
+
+	
+</head>
+<body>
+
+
+<?php
+
+	nav('index.php');
+
+?>
+
+
+
+<div id="wrap">
+
+	<h1>SOS<br>Community Resilience</h1>
+
+		<div class="textBox">
+			<p> SOS button to be used in times of
+				emergency.<br>Please click if you are in one of
+				the emergencys listed.
+			</p>
+		</div><!--textBox-->
+		
+
+
+
+	<div id="nav-SOS">
+				<li><a href="sms://+353879635141?body=I%20NEED%20HELP%20at%20this%20location%20" title="SOS">SOS</a></li>
+	</div><!--nav-SOS-->
+		
+</div><!--wrap-->
+
+
+</body>
+</html>
