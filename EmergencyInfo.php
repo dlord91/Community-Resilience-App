473bf3e0ba7aef9e7bfbbc34f870cbff6a210c58
David Lordan <110326755@umail.ucc.ie>
2014-02-23 15:38:23 +0000

    Create EmergencyInfo.php

---
+++ EmergencyInfo.php
@@ -0,0 +1,106 @@
+
+<?php
+
+ include_once'func.php';
+
+
+
+head('Emergency Information');
+
+
+?>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="javascript.js"></script>
+</head>
+<body>
+
+
+<?php
+
+	nav('index.php');
+
+?>
+
+
+
+<div id="wrap">
+
+	<h1>Emergency Information<br>Community Resilience</h1>
+
+		<div class="textBox">
+			<p></p>
+		</div><!--textBox-->
+		
+
+
+
+	<div id="content">
+	
+	
+		<div class="accordionBtn">
+							<h3>Severe Weather</h3>
+		</div><!--accordionBtn-->
+						
+						<div class="accordionCon">						
+			<p>Millennium Park - Located on the western outskirts of Naas, this exciting 400 acre development comprises a fully serviced office/high technology campus, comprising a range of retail offerings and enterprise space for early stage manufacturing and service enterprises.<br/><br/>With Millennium Park I was required to do a complete package, which included some features such as an advanced feature website, photography, logo design and mobile scripting. I added some features such as geolocation to give step by step directions from any location in the world. This website contains a lot of content but gives the opposite impression to give an uncluttered feel. Other scripts include retina images to insure it is ahead of current day display types.</p>
+							
+					</div><!--accordionCon-->
+					
+					
+					
+			<div class="accordionBtn">
+							<h3>Flooding</h3>
+		</div><!--accordionBtn-->
+						
+						<div class="accordionCon">						
+			<p>Millennium Park - Located on the western outskirts of Naas, this exciting 400 acre development comprises a fully serviced office/high technology campus, comprising a range of retail offerings and enterprise space for early stage manufacturing and service enterprises.<br/><br/>With Millennium Park I was required to do a complete package, which included some features such as an advanced feature website, photography, logo design and mobile scripting. I added some features such as geolocation to give step by step directions from any location in the world. This website contains a lot of content but gives the opposite impression to give an uncluttered feel. Other scripts include retina images to insure it is ahead of current day display types.</p>
+							
+					</div><!--accordionCon-->
+					
+					
+					<div class="accordionBtn">
+							<h3>Animal Disease</h3>
+		</div><!--accordionBtn-->
+						
+						<div class="accordionCon">						
+			<p>Millennium Park - Located on the western outskirts of Naas, this exciting 400 acre development comprises a fully serviced office/high technology campus, comprising a range of retail offerings and enterprise space for early stage manufacturing and service enterprises.<br/><br/>With Millennium Park I was required to do a complete package, which included some features such as an advanced feature website, photography, logo design and mobile scripting. I added some features such as geolocation to give step by step directions from any location in the world. This website contains a lot of content but gives the opposite impression to give an uncluttered feel. Other scripts include retina images to insure it is ahead of current day display types.</p>
+							
+					</div><!--accordionCon-->
+					
+					
+					
+					<div class="accordionBtn">
+							<h3>Terrorism</h3>
+		</div><!--accordionBtn-->
+						
+						<div class="accordionCon">						
+			<p>Millennium Park - Located on the western outskirts of Naas, this exciting 400 acre development comprises a fully serviced office/high technology campus, comprising a range of retail offerings and enterprise space for early stage manufacturing and service enterprises.<br/><br/>With Millennium Park I was required to do a complete package, which included some features such as an advanced feature website, photography, logo design and mobile scripting. I added some features such as geolocation to give step by step directions from any location in the world. This website contains a lot of content but gives the opposite impression to give an uncluttered feel. Other scripts include retina images to insure it is ahead of current day display types.</p>
+							
+					</div><!--accordionCon-->
+					
+					
+					
+					<div class="accordionBtn">
+							<h3>Major industrial Accident</h3>
+		</div><!--accordionBtn-->
+						
+						<div class="accordionCon">						
+			<p>Millennium Park - Located on the western outskirts of Naas, this exciting 400 acre development comprises a fully serviced office/high technology campus, comprising a range of retail offerings and enterprise space for early stage manufacturing and service enterprises.<br/><br/>With Millennium Park I was required to do a complete package, which included some features such as an advanced feature website, photography, logo design and mobile scripting. I added some features such as geolocation to give step by step directions from any location in the world. This website contains a lot of content but gives the opposite impression to give an uncluttered feel. Other scripts include retina images to insure it is ahead of current day display types.</p>
+							
+					</div><!--accordionCon-->
+					
+					
+					
+					
+					
+					
+						
+						
+			</div><!--content-->
+		
+</div><!--wrap-->
+
+
+</body>
+</html>
