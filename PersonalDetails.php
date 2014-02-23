8fb8a5ab9098396b9b933b2debfd45ee5e591591
David Lordan <110326755@umail.ucc.ie>
2014-02-23 15:52:17 +0000

    Create PersonalDetails.php

---
+++ PersonalDetails.php
@@ -0,0 +1,45 @@
+<html>
+	<head> 
+		<title>Personal Details</title>
+		<link rel = "stylesheet" type="text/css" 		
+			href = "sample.css"
+	
+	</style>
+	
+</head>
+
+<body>
+<head>
+
+<h1 style="text-align:center;"> Personal Details</h1>
+<h1 style="text-align:center;"> Community Resilience</h1>
+
+		<center>
+		<p> Here you will put in some basic important <br>
+			information about yourself in order to help those <br>
+			in your area help you. <br>
+		</center>
+		</div>
+	<center>
+		
+
+<div id="center">
+<div id="nav-menu">
+		<ul>
+		
+		<li><a href="Personal Details.html" title="Account">Medical Details</a></li>
+		<li><a href="Emergency Information.html" title="Emergency Info">Health Conditions</a></li>
+		<li><a href="People in Your Area.html" title="Local Residents">Next of Kin</a></li>
+		<li><a href="Local Authorities.html" title="Local Authorities">Family Health Details</a></li>
+		<li><a href="Local Map.html" title="CLocal Map">Directions to User's House</a></li>
+		</ul>
+		
+		</div> 
+		</div>
+		</div>
+		</div>
+		</center>
+
+</body>
+
+</html>
