0c0a7f57d4481106d5990e73b715f22a205a7a99
David Lordan <110326755@umail.ucc.ie>
2014-02-23 15:31:24 +0000

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+ include_once'func.php';
+
+
+
+head('Main Menu');
+
+
+?>
+
+
+
+
+</head>
+<body>
+
+<?php
+
+nav('index.php');
+
+?>
+
+
+
+<div id="wrap">
+
+	<h1>Community Resilience</h1>
+
+	<div id="nav-menu">
+
+			<li><a href="SOS.php" title="SOS">SOS</a></li></ul>
+			<li><a href="EmergencyInfo.php" title="Emergency Info">Emergency Information</a></li>
+			<li><a href="area.php" title="Local Residents">People in Your Area</a></li>
+			<li><a href="LocalAuthorities.php" title="Local Authorities">Local Authorities</a></li>
+			<li><a href="HelloWorld.php" title="Local Map">Local Map</a></li>
+		
+	</div><!--nav_menu-->
+</div><!--centre-->
+
+
+</body>
+</html>
